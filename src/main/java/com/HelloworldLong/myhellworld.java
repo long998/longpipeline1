b282f8090f3e83b426a4426d5a0c4d826a0aafb1
long998 <48188957+long998@users.noreply.github.com>
2019-03-04 09:13:40 +1100

    Create myhellworld.java

---
+++ src/main/java/com/HelloworldLong/myhellworld.java
@@ -0,0 +1,11 @@
+
+class Solution {
+
+  public static void main(String args[]) {
+
+    System.out.println("Hello World!,hello long, now test pipeline!");
+    System.out.println("the first hellworld path is src/main/java/com/HelloworldLong/");
+
+  }
+
+}
